8fa8d5822904c01de964fc4364d44a8b40f36a7a
Leah Lackner <leah.lackner+github@gmail.com>
2022-04-16 15:02:46 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from platform import python_version
+from setuptools import setup, find_packages
+import sys
+
+with open(".python-version", "r") as f:
+    pyver_str = f.read().strip()
+    pyver_tuple = tuple([int(x) for x in pyver_str.split(".")])
+    if sys.version_info < pyver_tuple:
+        sys.exit(f'Sorry, Python < {pyver_str} is not supported')
+
+with open("requirements.txt", "r") as f:
+    required_packages = f.read().strip().split()
+
+setup_info = dict(
+    name='evalsys',
+    version='0.0.1',
+    author='Leah Lackner',
+    author_email='leah.lackner+github@gmail.com',
+    url='https://github.com/evyli/evalsys',
+    project_urls={
+        'Documentation': 'https://github.com/evyli/evalsys/blob/master/README.md#evalsys',
+        'Source': 'https://github.com/evyli/evalsys',
+        'Tracker': 'https://github.com/evyli/evalsys/issues',
+    },
+    description='Tool to configure Linux based systems including population of dotfiles and package installation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    platforms='Linux, Mac OSX',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.10.3',
+    ],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'evalsys=evalsys.main:run_typer'
+        ],
+    },
+    packages=find_packages(),
+    install_requires=required_packages,
+)
+setup(**setup_info)
