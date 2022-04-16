758a6b4d259d5b57577e2c7f4651ffe29ee3d12d
Leah Lackner <leah.lackner+github@gmail.com>
2022-04-16 18:47:51 +0200

    Implement setup commands

--- setup.py
+++ setup.py
@@ -1,7 +1,66 @@
 #!/usr/bin/env python
+import pathlib
 from platform import python_version
 from setuptools import setup, find_packages
 import sys
+import distutils.cmd
+import distutils.log
+import subprocess
+import typing
+
+project_name = "evalsys"
+
+
+def find_all_source_files():
+    base_files = ["src", "tests"]
+    for file in base_files:
+        if file.endswith(".py"):
+            yield file
+        for path in pathlib.Path(file).rglob("*.py"):
+            yield str(path)
+
+
+def generate_simple_command(
+    bin: str,
+    desc: str,
+    args: typing.Iterable[str] | None = None,
+):
+    class SimpleCommand(distutils.cmd.Command):
+        description = desc
+        user_options: list[str] = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            command = [bin]
+
+            if args:
+                command += list(args)
+            self.announce(
+                f"Running command: {' '.join(command)}", level=distutils.log.INFO
+            )
+            subprocess.check_call(command)
+
+    return SimpleCommand
+
+
+MypyCommand = generate_simple_command(
+    "mypy", "run mypy on Python source files", find_all_source_files()
+)
+
+BlackCommand = generate_simple_command(
+    "black", "run black on Python source files", find_all_source_files()
+)
+
+TestCommand = generate_simple_command(
+    "pytest",
+    "run pytest",
+    [f"--cov={project_name}", str(pathlib.Path("tests").resolve())],
+)
 
 with open(".python-version", "r") as f:
     prj_pyver_str = f.read().strip()
@@ -15,7 +74,7 @@ with open("requirements.txt", "r") as f:
     required_packages = f.read().strip().split()
 
 setup_info = dict(
-    name="evalsys",
+    name=project_name,
     version="0.0.1",
     author="Leah Lackner",
     author_email="leah.lackner+github@gmail.com",
@@ -46,5 +105,10 @@ setup_info = dict(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=required_packages,
+    cmdclass={
+        "mypy": MypyCommand,
+        "black": BlackCommand,
+        "test": TestCommand,
+    },
 )
 setup(**setup_info)
